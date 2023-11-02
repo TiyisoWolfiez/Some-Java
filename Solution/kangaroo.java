6d8a4d2d36f36d89cf8ea8488463a191b279d0f8
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-11-02 23:24:33 +0200

    Update kangaroo.java

--- Solution/kangaroo.java
+++ Solution/kangaroo.java
@@ -1,1 +1,20 @@
-
+public static String kangaroo(int x1, int v1, int x2, int v2) {
+    // Write your code here
+    
+    String yes = "YES";
+    String no = "NO";
+    
+    if (v1 == v2) {
+        if (x1 == x2) {
+            return yes;
+        } else {
+            return no;
+        }
+    }
+    
+    if ((x2 - x1) % (v1 - v2) == 0 && (x2 - x1) / (v1 - v2) >= 0) {
+        return yes;
+    } else {
+        return no;
+    }
+}
