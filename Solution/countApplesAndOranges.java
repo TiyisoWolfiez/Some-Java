1334d3f7904da6ebe8a1bbc4d543c9ddde0e9608
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-11-02 23:19:10 +0200

    Update countApplesAndOranges.java

--- Solution/countApplesAndOranges.java
+++ Solution/countApplesAndOranges.java
@@ -1,1 +1,24 @@
-
+ public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+    // Write your code here
+    int appleCount = 0;
+    int orangeCount = 0;
+    
+    for(int k = 0; k < apples.size(); k++){
+        int posA = apples.get(k) + a;
+        
+        if(posA >= s && posA <= t){
+            appleCount++;
+        }
+    }
+    
+    for(int j = 0; j < oranges.size(); j++){
+        int posB = oranges.get(j) + b;
+        
+        if(posB >= s && posB <= t){
+            orangeCount++;
+        }
+    }
+    System.out.println(appleCount);
+    System.out.println(orangeCount);
+    
+    }
