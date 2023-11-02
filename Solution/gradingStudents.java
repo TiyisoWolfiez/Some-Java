020c454eba27162a9ef518f0d53061d3033fcc06
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-11-02 23:12:50 +0200

    Update gradingStudents.java

--- Solution/gradingStudents.java
+++ Solution/gradingStudents.java
@@ -1,1 +1,18 @@
-
+public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+    
+        List<Integer> rounded = new ArrayList<>();
+        
+        for (Integer grade : grades) {
+            int nextMultipleOf5 = (grade / 5 + 1) * 5;
+   
+            if(grade < 38){
+                rounded.add(grade);
+            }else if(nextMultipleOf5 - grade  < 3){
+                rounded.add(nextMultipleOf5);
+            } else{
+                rounded.add(grade);
+            }
+        }
+        return rounded;
+  }
