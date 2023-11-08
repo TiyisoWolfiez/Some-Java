1d276ca54f22ff2491d87555cc95db1d49f94c4d
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-11-09 00:09:15 +0200

    Update dayOfProgrammer.java

--- Solution/dayOfProgrammer.java
+++ Solution/dayOfProgrammer.java
@@ -1,1 +1,24 @@
-
+public static String dayOfProgrammer(int year) {
+    // Write your code here
+        if(year == 1918){
+            return "26.09.1918";
+        }
+        
+        String yy = String.valueOf(year);
+        String ddmmyy;
+        String ddmm = "12.09.";
+        String ddMM = "13.09.";
+        
+        if(year > 1918 && year % 400 == 0 || year % 4 == 0 && year % 100 != 0)      {
+            ddmmyy = ddmm.concat(yy);
+            return ddmmyy;
+        }
+        else if(year < 1917 && year % 4 == 0){
+                ddmmyy = ddmm.concat(yy);
+                return ddmmyy;
+        }
+        else{
+                ddmmyy = ddMM.concat(yy);
+                return ddmmyy;
+        } 
+}
