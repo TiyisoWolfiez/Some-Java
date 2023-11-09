880684f920657c55b34467125ab0cde2fee945da
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-11-09 08:21:27 +0200

    Update breakingRecords.java

--- Solution/breakingRecords.java
+++ Solution/breakingRecords.java
@@ -1,1 +1,25 @@
-
+public static List<Integer> breakingRecords(List<Integer> scores) {
+    // Write your code here
+        int most = 0;
+        int least = 0;
+        
+        int highestScore = scores.get(0);
+        int lowestScore = scores.get(0);
+        
+        for(int n : scores){
+            if(n > highestScore){
+                highestScore = n;
+                most++;
+            }
+            
+            if(n < lowestScore){
+                lowestScore = n;
+                least++;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        result.add(most);
+        result.add(least);
+        
+        return result;
+}
