ed2ace9f225ee732929aa75feb28ec44090a2118
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-11-10 21:53:50 +0200

    Update bonAppetit.java

--- Solution/bonAppetit.java
+++ Solution/bonAppetit.java
@@ -1,1 +1,18 @@
-
+public static void bonAppetit(List<Integer> bill, int k, int b) {
+    String bon = "Bon Appetit";
+    int Anna = bill.get(k);
+    int sum = 0;
+    
+    for(int n = 0; n < bill.size(); n++){
+        sum += bill.get(n);
+    }
+    
+    int total = (sum - Anna) / 2;
+    
+    if (total == b) {
+        System.out.println(bon);
+    } else {
+        int moneyOwed = Anna / 2;
+        System.out.println(moneyOwed);
+    }
+}
