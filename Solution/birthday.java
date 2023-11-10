77384973608de655844954918cfb309135764e09
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-11-10 21:48:57 +0200

    Update birthday.java

--- Solution/birthday.java
+++ Solution/birthday.java
@@ -1,1 +1,15 @@
+public static int birthday(List<Integer> s, int d, int m) {
+    int ways = 0;
 
+    for(int k = m - 1; k < s.size(); k++){
+        int sum = 0;
+        for (int i = k; i > k - m; i--) {
+            sum += s.get(i);
+        }
+        if(sum == d){
+            ways++;
+        }
+    }
+    
+    return ways;
+}
